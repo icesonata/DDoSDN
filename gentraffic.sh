b1f4d3521b395446db3a4c8ae047566953bd3488
Long Nguyen <hoanglong1622000@gmail.com>
2020-12-17 22:48:24 +0700

    first commit

---
+++ gentraffic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Simulate normal traffic
+while true;
+do
+    packets=$(shuf -i 10-20 -n 1)
+    bytes=$(shuf -i 150-200 -n 1)
+    delay=$(shuf -i 1-5 -n 1)
+    sudo hping3 -c $packets -d $bytes -s 80 -k 10.0.0.1
+    sleep $delay
+done
