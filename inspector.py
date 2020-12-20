4bd6855ce7833c809eb11122656bd5dedc0f5c82
Long Nguyen <hoanglong1622000@gmail.com>
2020-12-20 18:42:57 +0700

    add inspector that frequently keep track of network flows

---
+++ inspector.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, classification_report
+import joblib
+
+filename = 'classifier.sav'
+classifier = joblib.load(filename)
+dt_realtime = pd.read_csv('realtime.csv')
+result = classifier.predict(dt_realtime)
+
+with open('.result', 'w') as f:
+    f.write(str(result[0]))
