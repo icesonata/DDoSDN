5df14274223358a9f48167d631d233a8e905e0e9
Long Nguyen <hoanglong1622000@gmail.com>
2020-12-20 13:47:37 +0700

    change awk commands to suit re-customized controller's flow output structure

--- collect.sh
+++ collect.sh
@@ -4,13 +4,20 @@
 # ...
 # done
 
+# sudo ovs-ofctl dump-flows s1 > data/raw
 grep "nw_src" data/raw > data/flowentries.csv
+
+# Old ver
 awk -F "," '{split($4,a,"="); print a[2]","}' data/flowentries.csv > data/packets.csv
 awk -F "," '{split($5,b,"="); print b[2]","}' data/flowentries.csv > data/bytes.csv
-awk -F "," '{split($10,c,"="); print c[2]","}' data/flowentries.csv > data/ipsrc.csv
-awk -F "," '{split($11,d,"=");  split(d[2],e," "); print e[1]","}' data/flowentries.csv > data/ipdst.csv
+# awk -F "," '{split($10,c,"="); print c[2]","}' data/flowentries.csv > data/ipsrc.csv
+# awk -F "," '{split($11,d,"=");  split(d[2],e," "); print e[1]","}' data/flowentries.csv > data/ipdst.csv
+
+# Current ver
 # python computeTuples.py
 
+awk -F "," '{out=""; for(i=2;i<=NF;i++){out=out" "$i}; print out}' data/flowentries.csv | awk -F " " '{split($11,d,"="); print d[2]","}' > data/ipsrc.csv
+awk -F "," '{out=""; for(i=2;i<=NF;i++){out=out" "$i}; print out}' data/flowentries.csv | awk -F " " '{split($12,d,"="); print d[2]","}' > data/ipdst.csv
 
 
 
@@ -24,3 +31,7 @@ awk -F "," '{split($11,d,"=");  split(d[2],e," "); print e[1]","}' data/flowentr
 # ovs-ofctl reference
 # add-flow SWITCH FLOW        add flow described by FLOW    e.g. ... add-flow s1 "flow info"
 # add-flows SWITCH FILE       add flows from FILE           e.g. ... add-flows s1 flows.txt
+
+# default_flow=$(sudo ovs-ofctl dump-flows s1 | tail -n 1)    # Get action:CONTROLLER:<port_num>
+# sudo ovs-ofctl del-flows s1
+# sudo ovs-ofctl add-flow s1 "$default_flow"
