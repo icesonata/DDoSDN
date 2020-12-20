dfe0135101574cc1cebfd1db1f1fb867c040f811
Long Nguyen <hoanglong1622000@gmail.com>
2020-12-20 18:50:50 +0700

    complete svm model program

--- svm.py
+++ svm.py
@@ -4,17 +4,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.externals import joblib
+import joblib
 
 # Import the dataset
-df = pd.read_csv('data/Data.csv')
+df = pd.read_csv('data/dataset.csv')
 
 # Splitting dataset into features and label
 X = df.drop('Class', axis=1)
 y = df['Class']
 
 # Splitting the dataset into the training set and the test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 # Feature scaling (or standardization)
 scaler = StandardScaler()
@@ -22,7 +22,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Fitting SVM with the training set
-classifier = SVC(kernel='rbf', random_state=0)
+classifier = SVC(kernel='linear', random_state=0)
 classifier.fit(X_train, y_train)
 
 # Testing the model by classifying the test set
@@ -32,11 +32,11 @@ y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 cr = classification_report(y_test, y_pred)
 
-# Print out confusion matrix and report
-print(cm)
-print(cr)
+# # Print out confusion matrix and report
+# print(cm)
+# print(cr)
 
-# # Export model
-# filename = 'ml_model.sav'
-# joblib.dump(classifier, filename)
-# print("Model exported!")
+# Export model
+filename = 'classifier.sav'
+joblib.dump(classifier, filename)
+print("Model exported!")
