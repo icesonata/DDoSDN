350bb9e2b1ce3440d6f037d001f60590148d45cd
Long Nguyen <hoanglong1622000@gmail.com>
2021-02-19 00:23:32 +0700

    remove the redundancy

--- shortcut.sh
+++
@@ -1,4 +0,0 @@
-alias miniedit="sudo ~/mininet/examples/miniedit.py"
-
-# shorten PS1 display
-# export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$'
